10a04fba3ec4fc89ffb4d7d153e3422352d05648
[SW8] 이진주 <dsa0362@gmail.com>
2024-04-15 21:07:39 +0900

    ♻️refactor : .으로 이름변경,컬렉션 이름 설정 빼기

--- src/bookmark/bookmark.module.ts
+++ src/bookmark/bookmark.module.ts
@@ -1,7 +1,7 @@
 import { Module } from "@nestjs/common";
-import { BookmarkController } from "./bookmark-controller";
-import { BookmarkRepository } from "./bookmark-repository";
-import { BookmarkService } from "./bookmark-service";
+import { BookmarkController } from "./bookmark.controller";
+import { BookmarkRepository } from "./bookmark.repository";
+import { BookmarkService } from "./bookmark.service";
 
 
 @Module({
