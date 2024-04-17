20fe2ee3cc89eedd7b18df7e7a2b04199c36ab32
[SW8] 이진주 <dsa0362@gmail.com>
2024-04-17 10:54:46 +0900

    ✨feat :  응답 리스폰스 서비스 아닌 그냥

--- src/errors/response.format.ts
+++ src/errors/response.format.ts
@@ -1,8 +1,5 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class ResponseFormatService {
-  buildResponse(data: any, errorMessage?: string) {
+export class ResponseFormat {
+  static buildResponse(data: any, errorMessage?: string) {
     return {
       error: errorMessage ?? null,
       data,
