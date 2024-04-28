d66096438148235c0189e8902ac0ac6f37995975
parkjiwonnn <jiwonp05@gmail.com>
2024-04-28 16:24:02 +0900

    ✨feat : exception filter, response format 적용

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { GlobalExceptionFilter } from './global/exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api');
   const configService = app.get(ConfigService);
   const PORT = configService.get<number>('PORT');
+
+  app.useGlobalFilters(new GlobalExceptionFilter());
   await app.listen(PORT, () => {
     console.log(`✅ Server is running on port ${PORT}`);
   });
-  
 }
 bootstrap();
